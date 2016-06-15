89a49b41393c4c08df48d8f36de52dc7223a6b9f
AMSaC Lab <AMSaC Lab>
2016-06-15 15:15:25 -0400

    Initial port of TIMware to KL25Z platform, incorrect clock frequency but SPI commands are correct

---
+++ TIM_KL25Z/software/inc/constants.h
@@ -0,0 +1,47 @@
+/*
+ * queue
+ *
+ *  Created on: Mar 6, 2016
+ *      Author: Ian Bacus (ianbacus@gmail.com)
+ */
+
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+#include <stdint.h> //int definitions
+#include <stdbool.h>
+#include <stdlib.h> //calloc, free
+#include <stdint.h>
+
+#ifdef TIVA
+#include <sysctl.h>
+#include <gpio.h>
+#include <ssi.h>
+#include <interrupt.h>
+#include <hw_types.h>
+#include <hw_memmap.h>
+#include <hw_ints.h>
+#include <pin_map.h>
+#endif
+
+#define arrayLen(x)	(sizeof(x)/sizeof(x[0]))
+
+#define FRAME_TYPE	uint8_t
+#define FRAME_SIZE	(sizeof(FRAME_TYPE))
+#define FRAME_BITS	(8*FRAME_SIZE)
+#define DELAY 10000000
+#define IDLE_PAD 30
+
+//Supports 1 to 8 byte shift registers
+#define PREAMBLE_FRAME	((FRAME_TYPE)(0x55))
+#define PREAMBLE_LEN	(4/FRAME_SIZE)
+
+typedef uint8_t engine_t;
+typedef enum
+{
+	Reset,Idle,Preamble, LocoA, LocoB, TurnA, TurnB
+} TX_state_t;
+
+
+#endif
+
+
