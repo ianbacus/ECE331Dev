a9d6c23458c7211b3916583ec19bea781bf3a306
AMSaC Lab <AMSaC Lab>
2017-01-22 14:03:34 -0500

    Tested code for student-side DCC control.

---
+++ TIM_KL25Z4/software/inc/DCC.h
@@ -0,0 +1,12 @@
+
+#ifndef __HEADER__
+
+#define __HEADER__
+
+
+void DCC_init();
+void set_engineB(int data);
+void set_engineA(int data);
+void set_turnouts(int data);
+void delay(unsigned int d);
+#endif
